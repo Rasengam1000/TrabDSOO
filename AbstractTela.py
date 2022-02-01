97233ee4b38e859039f7246b66fe162cc816f107
Jose Carlos <zezinho158@gmail.com>
2022-02-01 15:38:23 -0300

    começando abstracttela

--- AbstractTela.py
+++ AbstractTela.py
@@ -1,1 +1,21 @@
-apenas testando
+from abc import ABC, abstractmethod
+
+class AbstractTela(ABC):
+
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    def verifica_num_int(self, mensagem: str = '', inteiros_validos: [] = None):
+        while True:
+            valor_lido = input(mensagem)
+            try:
+                inteiro = int(valor_lido)
+                if inteiros_validos and inteiro not in inteiros_validos:
+                    raise ValueError
+                return inteiro
+            except ValueError:
+                print('Valor incorreto: Digite um numero inteiro valido')
+                if inteiros_validos:
+                    print('Valores Validos: ', inteiros_validos)
+
