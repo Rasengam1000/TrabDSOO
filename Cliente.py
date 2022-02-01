20bee4b08847fd7fb2ac0b05ecb4d6e588b84d5c
Jose Carlos <zezinho158@gmail.com>
2022-02-01 17:16:16 -0300

    fazendo parte do cliente e tela principal

--- Cliente.py
+++ Cliente.py
@@ -1,1 +1,43 @@
-a
+from abc import ABC, abstractmethod
+
+class Cliente(ABC):
+
+    @abstractmethod
+    def __init__(self, nome: str, sobrenome: str, cpf: int, idade: int):
+        self.__nome = nome
+        self.__sobrenome = sobrenome
+        self.__cpf = cpf
+        self.__idade = idade
+        #criar o carrinho
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, nome: str):
+        self.__nome = nome
+
+    @property
+    def sobrenome(self):
+        return self.__sobrenome
+
+    @sobrenome.setter
+    def sobrenome(self, sobrenome: str):
+        self.__sobrenome = sobrenome
+
+    @property
+    def cpf(self):
+        return self.__cpf
+
+    @cpf.setter
+    def cpf(self, cpf: int):
+        self.__cpf = cpf
+
+    @property
+    def idade(self):
+        return self.__idade
+
+    @idade.setter
+    def idade(self, idade: int):
+        self.__idade = idade
