08e270b54c853b1c0999b263dcd95c525b62dc2a
Jose Carlos <zezinho158@gmail.com>
2022-02-02 17:32:38 -0300

    criadas funcoes de pegar info, mostra mensagem, mostrar info, listar cliente, incluir cliente

--- TelaClientes.py
+++ TelaClientes.py
@@ -16,3 +16,25 @@ class TelaClientes(AbstractTela):
         print('0 - Retornar')
         opcao = self.verifica_num_int('Escolha a opcao: ', [1, 2, 3, 4, 5, 0])
         return opcao
+
+    def pegar_info(self): #ver se precisa verificar
+        print('Dados Cliente')
+        nome = input('Nome: ')
+        sobrenome = input('Sobrenome: ')
+        cpf = input('CPF: ')
+        idade = input('Idade: ')
+
+        return{'nome': nome, 'sobrenome': sobrenome, 'cpf': cpf, 'idade': idade}
+
+    def mostra_mensagem(self, msg):  #pode ser util colocar na abstract
+        print(msg)
+
+    def mostrar_info(self, dados_cliente):
+        print('Nome do Cliente: ', dados_cliente['nome'])
+        print('Sobrenome do Cliente ', dados_cliente['sobrenome'])
+        print('CPF do Cliente ', dados_cliente['cpf'])
+        print('Idade do Cliente ', dados_cliente['idade'])
+        print()   #espaco em branco
+
+    def selecionar_cliente(self): #continuar daqui
+        pass
