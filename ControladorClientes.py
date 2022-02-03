8c687df48057688111f0df0f14cb9b59a1097e33
Jose Carlos <zezinho158@gmail.com>
2022-02-03 17:25:04 -0300

    mudando um pouco

--- ControladorClientes.py
+++ ControladorClientes.py
@@ -24,12 +24,11 @@ class ControladorClientes:
                                                   'cpf': cliente.cpf, 'idade': cliente.idade})
 
     def excluir_cliente(self):
-        self.listar_cliente()
         cpf_cliente = self.__telaclientes.selecionar_cliente()
         cliente = self.escolhe_cliente_por_cpf(cpf_cliente)
         if cliente is not None:
             self.__clientes.remove(cliente)
-            self.listar_cliente()
+            self.__telaclientes.mostra_mensagem('Cliente excluido com sucesso')
         else:
             self.__telaclientes.mostra_mensagem('Cliente nao existente')
 
@@ -40,7 +39,6 @@ class ControladorClientes:
         return None
 
     def alterar_cadastro(self):
-        self.listar_cliente()
         cpf_cliente = self.__telaclientes.selecionar_cliente()
         cliente = self.escolhe_cliente_por_cpf(cpf_cliente)
         if cliente is not None:
@@ -49,7 +47,7 @@ class ControladorClientes:
             cliente.sobrenome = novo_cadastro['sobrenome']
             cliente.cpf = novo_cadastro['cpf']
             cliente.idade = novo_cadastro['idade']
-            self.listar_cliente()
+            self.__telaclientes.mostra_mensagem('Cadastro alterado com sucesso')
         else:
             self.__telaclientes.mostra_mensagem('Cliente nao existente')
 
@@ -63,12 +61,11 @@ class ControladorClientes:
             self.__telaclientes.mostra_mensagem('Cliente nao existente')
 
     def retornar(self):
-        self.__controladorprincipal.abre_tela() #verificar como esta o nome la
+        self.__controladorprincipal.abre_tela()  #verificar como esta o nome la
 
-    def abre_tela(self): #verificar se precisa dos parenteses nas opcoes
+    def abre_tela(self):  #verificar se precisa dos parenteses nas opcoes
         lista_opcoes = {1: self.incluir_cliente(), 2: self.excluir_cliente(), 3: self.alterar_cadastro(),
                         4: self.info_cliente(), 5: self.listar_cliente(), 0: self.retornar()}
 
-        continua = True
-        while continua:
+        while True:
             lista_opcoes[self.__telaclientes.opcoes()]()
